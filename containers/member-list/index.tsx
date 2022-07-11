d38f9fd0b5669c230fdcc5dfad73d277170c7f03
adrihle <adrian.lpes@gmail.com>
2022-07-11 05:47:07 +0200

    fix: offset in pagination

--- containers/member-list/index.tsx
+++ containers/member-list/index.tsx
@@ -1,23 +1,29 @@
 import { useModal, usePagination } from "./hooks";
-import { Pagination, Spin } from "antd";
+import { Empty, Pagination, Spin } from "antd";
 import { IMember } from "interfaces";
 import { MemberCard, MemberModal } from './components';
 import { Wrapper, CardWrapper } from './style';
 import { useResponsive } from "@hooks";
+import { PAGE_SIZE } from "@constants";
 
 interface Props {
   members: IMember[];
-}
+};
+
 
 export const MemberListContainer: React.FC<Props> = ({ members }) => {
   const { isLoading, page, navigate } = usePagination();
   const { isOpen, setMember, currentMember, setOpenModal } = useModal();
   const { grid } = useResponsive();
 
+  const isEmpty = !isLoading && !members;
+
+  const getTotal = (page: number) => page*PAGE_SIZE + 1;
+
   return (
     <>
       <Wrapper>
-        <Pagination current={page} onChange={navigate} pageSize={12} total={120} simple disabled={isLoading} />
+        <Pagination current={page} onChange={navigate} pageSize={PAGE_SIZE} total={getTotal(page)} simple disabled={isLoading} />
       </Wrapper>
       <Wrapper {...{isLoading}}>
         {isLoading && <Spin size="large"/>}
@@ -26,6 +32,7 @@ export const MemberListContainer: React.FC<Props> = ({ members }) => {
             {members.map(member => <MemberCard key={member.id} {...{member, setOpenModal, setMember}} />)}
           </CardWrapper>
         )}
+        {isEmpty && <Empty />}
         {currentMember && <MemberModal {...{isOpen, member: currentMember, setOpenModal}}/>}
       </Wrapper>
     </>
